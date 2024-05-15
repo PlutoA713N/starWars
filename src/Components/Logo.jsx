656239186de48d39827cf149bb8c6bbd48f26b1d
PlutoA713N <mallachandu99@gmail.com>
2024-05-15 18:32:47 +0530

    Import logo

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Typography, Box } from '@mui/material';
+import img from './../../public/i-made-a-neon-version-of-the-star-wars-logo-v0-nllri433rgs81.webp'
 
 
 const Logo = () => {
@@ -7,7 +8,7 @@ const Logo = () => {
     <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' , backgroundColor: 'transparent',}}>
       <Typography variant="h4" component="h1" sx={{ color: 'white', fontWeight: 'bold', marginRight: '10px' }}>
       </Typography>
-      <img src="../public/i-made-a-neon-version-of-the-star-wars-logo-v0-nllri433rgs81.webp" alt="Star Wars Logo" style={{ width: '20rem', height: '10rem' }} />
+      <img src={img} alt="Star Wars Logo" style={{ width: '20rem', height: '10rem' }} />
     </Box>
   );
 }
