d623f6a741a7b82e2650ca4668f74f903933613d
PlutoA713N <mallachandu99@gmail.com>
2024-05-15 16:05:31 +0530

    Import colors

--- src/Components/CardComponent.jsx
+++ src/Components/CardComponent.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Card, CardContent, Typography, CardMedia, Box } from '@mui/material';
 import ModalComponent from './ModalComponent';
 import Loading from './Loading';
+import colors from './colors';
 
 const CardComponent = ({ index, title, speciesColor, charactersData }) => {
   const [open, setOpen] = useState(false);
@@ -19,11 +20,7 @@ const CardComponent = ({ index, title, speciesColor, charactersData }) => {
 
   if (speciesColor.includes(',')) {
     color = speciesColor.split(',')[0].trim();
-  } else if (speciesColor === 'light') {
-    color = '#ffe0bd';
-  } else if (speciesColor === 'fair') {
-    color = '#fff5e1';
-  } else {
+  }else {
     color = speciesColor;
   }
 
@@ -40,6 +37,7 @@ const CardComponent = ({ index, title, speciesColor, charactersData }) => {
             transform: 'scale(1.05)',
           },
           border: '1px solid black',
+          bgcolor: colors[color],
         }}
         onClick={handleOpen}
       >
@@ -53,7 +51,7 @@ const CardComponent = ({ index, title, speciesColor, charactersData }) => {
             height: '350',
             objectFit: 'contain',
             objectPosition: 'center',
-            bgcolor: color,
+            // bgcolor: color,
             boxSizing: 'border-box',
             p: 2,
             transition: 'transform 0.3s',
