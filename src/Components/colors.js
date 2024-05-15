72874b7ec19fbdb62e1e46475d3e69def7e27144
PlutoA713N <mallachandu99@gmail.com>
2024-05-15 16:07:00 +0530

    Update colors object

---
+++ src/Components/colors.js
@@ -0,0 +1,25 @@
+const colors = {
+    fair: '#FADBD8',
+    gold: '#FFD700',
+    'white': '#FFFFFF',
+    'blue': '#ADD8E6',
+    'light': '#F0FFFF',
+    'red': '#FFC0CB',
+    'unknown': '#A9A9A9',
+    'green': '#7FFF00',
+    'green-tan': '#BDB76B',
+    'brown': '#A52A2A',
+    'pale': '#FFE4B5',
+    'metal': '#B0C4DE',
+    'dark': '#2F4F4F',
+    'brown mottle': '#8B4513',
+    'grey': '#808080',
+    'mottled green': '#556B2F',
+    'orange': '#FFA500',
+    'tan': '#D2B48C',
+    'silver': '#C0C0C0',
+  }
+  
+    
+    export default colors;
+    
