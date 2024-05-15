51011e7e4693b499a3c9cc1e9b8eedccb18fd944
PlutoA713N <mallachandu99@gmail.com>
2024-05-15 11:36:07 +0530

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import DisplayCharacters from './Components/DisplayCharacters';
+import { Box } from '@mui/material';
+import PaginatedItems from './Components/PaginatedItems';
+import SearchByName from './Components/SearchByName';
+
+
+function App() {
+  const [url, setUrl] = useState(import.meta.env.VITE_APP_GET_ALL_CHARACTERS);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const handleUrl = (newUrlLink) => {
+    setUrl(newUrlLink);
+  };
+
+  const handleTotalPages = (pages) => {
+    setTotalPages(pages);
+  };
+
+
+  const handleSearchUrl = (newUrlLink) => {
+    setUrl(newUrlLink);
+  }
+
+  return (
+    <Box sx={{ boxSizing: 'border-box', p: 6 }}>
+      <SearchByName  handleSearchUrl={handleSearchUrl}/>
+      <DisplayCharacters URL={url} handleTotalPages={handleTotalPages} />
+      <PaginatedItems handleUrl={handleUrl} totalPages={totalPages} />
+    </Box>
+  );
+}
+
+export default App;
+
+
