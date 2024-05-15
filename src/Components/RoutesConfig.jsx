7f838c93f739d01aa9c617888a06058784b50ece
PlutoA713N <mallachandu99@gmail.com>
2024-05-15 11:38:49 +0530

    Add RoutesConfig Component

---
+++ src/Components/RoutesConfig.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';import App from "./../App";
+
+const RoutesConfig = () => {
+    return(
+    <Router>
+        <Routes>
+        <Route path="/*" element={<Navigate to="/people" replace />} />
+                <Route default path='/people/' element={<App />} />
+        </Routes>
+    </Router>
+    )
+}
+
+export default RoutesConfig
+
