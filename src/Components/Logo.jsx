fc7816385e79dcd05cc6a1be142b5e38632b87fb
PlutoA713N <mallachandu99@gmail.com>
2024-05-15 18:26:12 +0530

    Update image path

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -7,7 +7,7 @@ const Logo = () => {
     <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' , backgroundColor: 'transparent',}}>
       <Typography variant="h4" component="h1" sx={{ color: 'white', fontWeight: 'bold', marginRight: '10px' }}>
       </Typography>
-      <img src="./../public/i-made-a-neon-version-of-the-star-wars-logo-v0-nllri433rgs81.webp" alt="Star Wars Logo" style={{ width: '20rem', height: '10rem' }} />
+      <img src="../public/i-made-a-neon-version-of-the-star-wars-logo-v0-nllri433rgs81.webp" alt="Star Wars Logo" style={{ width: '20rem', height: '10rem' }} />
     </Box>
   );
 }
