5aa49136ca635e0a38c12679275337b2f186d09f
PlutoA713N <mallachandu99@gmail.com>
2024-05-17 10:37:01 +0530

    Import Index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import './index.css'
 import RoutesConfig from './Components/RoutesConfig.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
