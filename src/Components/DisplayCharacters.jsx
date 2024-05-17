5e3269e163eee4ceca863a8cb14d04cf8046edf3
PlutoA713N <mallachandu99@gmail.com>
2024-05-17 12:23:05 +0530

    Update index

--- src/Components/DisplayCharacters.jsx
+++ src/Components/DisplayCharacters.jsx
@@ -44,24 +44,22 @@ const DisplayCharacters = ({ URL , handleTotalPages}) => {
   }
   
 
-  const previousPage = data?.previous ? Number(data.previous.match(/page=(\d+)/)[1]) : 0;
-
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'row',  flexWrap: 'wrap' , minHeight: '100vh'}}>
-      {data?.results.map((character, index) => (
-        <CardComponent
+      {data?.results.map((character, index) => {
+        console.log(character.url.match(/\/people\/(\d+)\//)[1], URL, 44)
+        return <CardComponent
           key={character.name}
           title={character.name}
           index={
-            !data?.previous && !data?.next
-              ? Number(data?.results[0].url.match(/\/people\/(\d+)\//)?.[1]) - 1
-              : previousPage ? previousPage * 10 + index + 1 : index + 1
+             character.url.match(/\/people\/(\d+)\//)[1]
+
           }
           charactersData={character}
           speciesColor={character.skin_color}
         />
-      ))}
+  })}
     </Box>
   );
 };
